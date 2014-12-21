fd8b41c562e614bb4f4f3d038a81e34d66145a90
jolamola <mrdanielkim@gmail.com>
2014-12-21 13:15:48 -0800

    fixes draw game issue

--- js/controller.js
+++ js/controller.js
@@ -10,7 +10,8 @@ angular
 		gameCtrl.blueWinCounter = 0;
 		gameCtrl.ojWinCounter = 0;
 		gameCtrl.clickBox = clickBox;
-		gameCtrl.restartButton = restartButton;
+		gameCtrl.rematchButton = rematchButton;
+		gameCtrl.newMatchButton = newMatchButton;
 		gameCtrl.setNewGameboard = setNewGameboard;
 		gameCtrl.buttonResetScore = buttonResetScore;
 
@@ -20,8 +21,7 @@ angular
 			gameCtrl.gameBoard = [
 				{value:"empty",setBlue:false,setOj:false}, {value:"empty",setBlue:false, setOj:false}, {value:"empty",setBlue:false,setOj:false}, 
 				{value:"empty",setBlue:false,setOj:false}, {value:"empty",  setBlue: false, setOj: false}, {value:"empty",  setBlue: false, setOj: false}, 
-				{value:"empty",  setBlue: false, setOj: false}, {value:"empty",  setBlue: false, setOj: false}, {value:"empty",  setBlue: false, setOj: false}
-				];
+				{value:"empty",  setBlue: false, setOj: false}, {value:"empty",  setBlue: false, setOj: false}, {value:"empty",  setBlue: false, setOj: false}];
 		}	
 
 		function clickBox(index) {  
@@ -40,8 +40,9 @@ angular
 							gameCtrl.gameBoard[0].value=="x" && gameCtrl.gameBoard[4].value=="x" && gameCtrl.gameBoard[8].value=="x" ||
 							gameCtrl.gameBoard[2].value=="x" && gameCtrl.gameBoard[4].value=="x" && gameCtrl.gameBoard[6].value=="x"
 							){
-							gameCtrl.blueWin=true;
-							gameCtrl.stopsClickFunc = "true";
+							gameCtrl.gameoverBox = true;
+							gameCtrl.blueWin = "Blue Wins!";
+							gameCtrl.stopsClickFunc = true;
 							gameCtrl.blueWinCounter ++;
 							}
 				} else {
@@ -57,8 +58,9 @@ angular
 							gameCtrl.gameBoard[0].value=="o" && gameCtrl.gameBoard[4].value=="o" && gameCtrl.gameBoard[8].value=="o" ||
 							gameCtrl.gameBoard[2].value=="o" && gameCtrl.gameBoard[4].value=="o" && gameCtrl.gameBoard[6].value=="o" 	
 							){
-							gameCtrl.ojWin=true;
-							gameCtrl.stopsClickFunc = "true";
+							gameCtrl.gameoverBox = true;
+							gameCtrl.ojWin = "Orange Wins!";
+							gameCtrl.stopsClickFunc = true;
 							gameCtrl.ojWinCounter ++;
 							} 
 				} 
@@ -66,19 +68,22 @@ angular
 				gameCtrl.firstClick = gameCtrl.firstClick == "x" ? "o" : "x";	
 				gameCtrl.drawCounter++;
 
-				if (gameCtrl.drawCounter >= 9){
-					gameCtrl.tie = true;
+				if (gameCtrl.drawCounter>=9 && gameCtrl.blueWin!="Blue Wins!" && gameCtrl.ojWin!="Orange Wins!"){
+					gameCtrl.gameoverBox = true;
+					gameCtrl.drawMsg = "Draw Game!";
+					gameCtrl.stopsClickFunc = true;
 				}
 
 			} else {}
 		}
 
-		function restartButton(){
+		function rematchButton(){
 			gameCtrl.setNewGameboard();
-			gameCtrl.blueWin=false;
-			gameCtrl.ojWin=false;
-			gameCtrl.tie=false;
-			gameCtrl.firstClick= "x";
+			gameCtrl.blueWin = "";
+			gameCtrl.ojWin = "";
+			gameCtrl.drawMsg = "";
+			gameCtrl.gameoverBox = false;
+			gameCtrl.firstClick = "x";
 			gameCtrl.stopsClickFunc = false;
 			gameCtrl.drawCounter = 0;
 		}
@@ -87,6 +92,12 @@ angular
 			gameCtrl.blueWinCounter = 0;
 			gameCtrl.ojWinCounter = 0;
 		}
+
+		function newMatchButton() {
+			rematchButton();
+			buttonResetScore();
+		}
+		
 }
 
 
