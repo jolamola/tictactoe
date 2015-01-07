44e2f78ac3ed620be7f37ae1fefc7c8dac0a9292
jolamola <mrdanielkim@gmail.com>
2015-01-06 16:38:46 -0800

    firebase add

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 angular
-	.module("myApp",[]);
+	.module("myApp",['firebase']);
 
 
 
--- js/controller.js
+++ js/controller.js
@@ -2,83 +2,126 @@ angular
 	.module("myApp")
 	.controller("GameController", GameController);
 
-	function GameController(){
+	GameController.$inject = ["$firebase"];
+
+
+	function GameController($firebase){
+
 		var gameCtrl = this;
+
 		gameCtrl.stopsClickFunc = false;
 		gameCtrl.firstClick = "x";
 		gameCtrl.drawCounter = 0;
 		gameCtrl.blueWinCounter = 0;
 		gameCtrl.ojWinCounter = 0;
-		gameCtrl.clickBox = clickBox;
+		gameCtrl.initGame = initGame;
+
+
 		gameCtrl.rematchButton = rematchButton;
 		gameCtrl.newMatchButton = newMatchButton;
-		gameCtrl.setNewGameboard = setNewGameboard;
 		gameCtrl.buttonResetScore = buttonResetScore;
 
-		gameCtrl.setNewGameboard();
-		
-		function setNewGameboard() {
-			gameCtrl.gameBoard = [
-				{value:"empty",setBlue:false,setOj:false}, {value:"empty",setBlue:false, setOj:false}, {value:"empty",setBlue:false,setOj:false}, 
-				{value:"empty",setBlue:false,setOj:false}, {value:"empty",  setBlue: false, setOj: false}, {value:"empty",  setBlue: false, setOj: false}, 
-				{value:"empty",  setBlue: false, setOj: false}, {value:"empty",  setBlue: false, setOj: false}, {value:"empty",  setBlue: false, setOj: false}];
-		}	
+		gameCtrl.playerOneTurn = true;
 
-		function clickBox(index) {  
-			if (gameCtrl.gameBoard[index].value === "empty" && gameCtrl.stopsClickFunc === false) {
-				gameCtrl.gameBoard[index].value = gameCtrl.firstClick;
-				if (gameCtrl.gameBoard[index].value === "x") {
-					gameCtrl.gameBoard[index].setBlue = true;
-					gameCtrl.gameBoard[index].setOj = false;
+		gameCtrl.winLogic = winLogic;
+		
+		function initGame(){
+			var ref = new Firebase("https://molattt.firebaseio.com/games");
+			var gameData = $firebase(ref).$asObject();	
+			return gameData;
+		}
+
+		gameCtrl.gameData = initGame();
+		var g = gameCtrl.gameData;
+
+		g.gameBoard = [
+			{value: "empty", setBlue: false, setOj: false}, {value: "empty", setBlue: false, setOj: false}, {value: "empty", setBlue: false, setOj: false}, 
+			{value: "empty", setBlue: false, setOj: false}, {value: "empty", setBlue: false, setOj: false}, {value: "empty", setBlue: false, setOj: false}, 
+			{value: "empty", setBlue: false, setOj: false}, {value: "empty", setBlue: false, setOj: false}, {value: "empty", setBlue: false, setOj: false}
+		];
+		g.$save();
+
+		function winLogic(index) {  
+			if (g.gameBoard[index].value === "empty" && gameCtrl.stopsClickFunc === false) {
+
+// neeed to track player turn
+//				if (gameCtrl.playerOneTurn === true) {
+//					g.gameBoard[index].value = gameCtrl.firstClick;
+//				} else {
+
+//				}
+
+
+				// sets color to blue
+				if (g.gameBoard[index].value === "x") {
+					g.gameBoard[index].setBlue = true;
+					g.gameBoard[index].setOj = false;
+					g.$save();
+
+						// checks for win
 						if (
-							gameCtrl.gameBoard[0].value=="x" && gameCtrl.gameBoard[1].value=="x" && gameCtrl.gameBoard[2].value=="x" || 
-							gameCtrl.gameBoard[3].value=="x" && gameCtrl.gameBoard[4].value=="x" && gameCtrl.gameBoard[5].value=="x" || 
-							gameCtrl.gameBoard[6].value=="x" && gameCtrl.gameBoard[7].value=="x" && gameCtrl.gameBoard[8].value=="x" ||
-							gameCtrl.gameBoard[0].value=="x" && gameCtrl.gameBoard[3].value=="x" && gameCtrl.gameBoard[6].value=="x" ||
-							gameCtrl.gameBoard[1].value=="x" && gameCtrl.gameBoard[4].value=="x" && gameCtrl.gameBoard[7].value=="x" ||
-							gameCtrl.gameBoard[2].value=="x" && gameCtrl.gameBoard[5].value=="x" && gameCtrl.gameBoard[8].value=="x" ||
-							gameCtrl.gameBoard[0].value=="x" && gameCtrl.gameBoard[4].value=="x" && gameCtrl.gameBoard[8].value=="x" ||
-							gameCtrl.gameBoard[2].value=="x" && gameCtrl.gameBoard[4].value=="x" && gameCtrl.gameBoard[6].value=="x"
+							g.gameBoard[0].value=="x" && g.gameBoard[1].value=="x" && g.gameBoard[2].value=="x" || 
+							g.gameBoard[3].value=="x" && g.gameBoard[4].value=="x" && g.gameBoard[5].value=="x" || 
+							g.gameBoard[6].value=="x" && g.gameBoard[7].value=="x" && g.gameBoard[8].value=="x" ||
+							g.gameBoard[0].value=="x" && g.gameBoard[3].value=="x" && g.gameBoard[6].value=="x" ||
+							g.gameBoard[1].value=="x" && g.gameBoard[4].value=="x" && g.gameBoard[7].value=="x" ||
+							g.gameBoard[2].value=="x" && g.gameBoard[5].value=="x" && g.gameBoard[8].value=="x" ||
+							g.gameBoard[0].value=="x" && g.gameBoard[4].value=="x" && g.gameBoard[8].value=="x" ||
+							g.gameBoard[2].value=="x" && g.gameBoard[4].value=="x" && g.gameBoard[6].value=="x"
 							){
 							gameCtrl.gameoverBox = true;
 							gameCtrl.blueWin = "Blue Wins!";
 							gameCtrl.stopsClickFunc = true;
 							gameCtrl.blueWinCounter ++;
+							g.$save();
 							}
 				} else {
-					gameCtrl.gameBoard[index].setBlue = false;
-					gameCtrl.gameBoard[index].setOj = true;
+					g.gameBoard[index].setBlue = false;
+					g.gameBoard[index].setOj = true;
+					g.$save();
 						if (
-							gameCtrl.gameBoard[0].value=="o" && gameCtrl.gameBoard[1].value=="o" && gameCtrl.gameBoard[2].value=="o" || 
-							gameCtrl.gameBoard[3].value=="o" && gameCtrl.gameBoard[4].value=="o" && gameCtrl.gameBoard[5].value=="o" || 
-							gameCtrl.gameBoard[6].value=="o" && gameCtrl.gameBoard[7].value=="o" && gameCtrl.gameBoard[8].value=="o" ||
-							gameCtrl.gameBoard[0].value=="o" && gameCtrl.gameBoard[3].value=="o" && gameCtrl.gameBoard[6].value=="o" ||
-							gameCtrl.gameBoard[1].value=="o" && gameCtrl.gameBoard[4].value=="o" && gameCtrl.gameBoard[7].value=="o" ||
-							gameCtrl.gameBoard[2].value=="o" && gameCtrl.gameBoard[5].value=="o" && gameCtrl.gameBoard[8].value=="o" ||
-							gameCtrl.gameBoard[0].value=="o" && gameCtrl.gameBoard[4].value=="o" && gameCtrl.gameBoard[8].value=="o" ||
-							gameCtrl.gameBoard[2].value=="o" && gameCtrl.gameBoard[4].value=="o" && gameCtrl.gameBoard[6].value=="o" 	
+							g.gameBoard[0].value=="o" && g.gameBoard[1].value=="o" && g.gameBoard[2].value=="o" || 
+							g.gameBoard[3].value=="o" && g.gameBoard[4].value=="o" && g.gameBoard[5].value=="o" || 
+							g.gameBoard[6].value=="o" && g.gameBoard[7].value=="o" && g.gameBoard[8].value=="o" ||
+							g.gameBoard[0].value=="o" && g.gameBoard[3].value=="o" && g.gameBoard[6].value=="o" ||
+							g.gameBoard[1].value=="o" && g.gameBoard[4].value=="o" && g.gameBoard[7].value=="o" ||
+							g.gameBoard[2].value=="o" && g.gameBoard[5].value=="o" && g.gameBoard[8].value=="o" ||
+							g.gameBoard[0].value=="o" && g.gameBoard[4].value=="o" && g.gameBoard[8].value=="o" ||
+							g.gameBoard[2].value=="o" && g.gameBoard[4].value=="o" && g.gameBoard[6].value=="o" 	
 							){
 							gameCtrl.gameoverBox = true;
 							gameCtrl.ojWin = "Orange Wins!";
 							gameCtrl.stopsClickFunc = true;
 							gameCtrl.ojWinCounter ++;
+							g.$save();
 							} 
 				} 
 
 				gameCtrl.firstClick = gameCtrl.firstClick == "x" ? "o" : "x";	
 				gameCtrl.drawCounter++;
+				g.$save();
 
 				if (gameCtrl.drawCounter>=9 && gameCtrl.blueWin!="Blue Wins!" && gameCtrl.ojWin!="Orange Wins!"){
 					gameCtrl.gameoverBox = true;
 					gameCtrl.drawMsg = "Draw Game!";
 					gameCtrl.stopsClickFunc = true;
+					g.$save();
 				}
 
 			} else {}
 		}
 
+
+		/********************************************		
+		* Game reset   
+		********************************************/
+
 		function rematchButton(){
-			gameCtrl.setNewGameboard();
+			g.gameBoard = [
+				{value: "empty", setBlue: false, setOj: false}, {value: "empty", setBlue: false, setOj: false}, {value: "empty", setBlue: false, setOj: false}, 
+				{value: "empty", setBlue: false, setOj: false}, {value: "empty", setBlue: false, setOj: false}, {value: "empty", setBlue: false, setOj: false}, 
+				{value: "empty", setBlue: false, setOj: false}, {value: "empty", setBlue: false, setOj: false}, {value: "empty", setBlue: false, setOj: false}
+			];
 			gameCtrl.blueWin = "";
 			gameCtrl.ojWin = "";
 			gameCtrl.drawMsg = "";
@@ -86,6 +129,7 @@ angular
 			gameCtrl.firstClick = "x";
 			gameCtrl.stopsClickFunc = false;
 			gameCtrl.drawCounter = 0;
+			g.$save();
 		}
 
 		function buttonResetScore(){
@@ -97,10 +141,4 @@ angular
 			rematchButton();
 			buttonResetScore();
 		}
-		
-}
-
-
-
-
-
+	}
